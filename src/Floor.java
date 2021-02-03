8625eefb3dbf7c16a8eaf1046be57f23d7339dfb
campbelldw <campdw@rogers.com>
2021-02-03 16:24:29 -0500

    created a seperate Direction file

--- src/Floor.java
+++ src/Floor.java
@@ -3,8 +3,43 @@ public class Floor {
 	private int floorNumber;
 	private FloorButton[] buttons;
 	
-	public Floor(int floorNumber, FloorButton[] buttons) { // a top floor variable is needed to know if the sub system is on the top floor
+	public Floor(int floorNumber, int buttonNumber) { // a top floor variable is needed to know if the sub system is on the top floor
 		this.floorNumber = floorNumber;
-		this.buttons = buttons;
+		if (buttonNumber == 1) {
+			if (floorNumber == 1) {
+				this.buttons = new FloorButton[] {new FloorButton(Direction.UP)};
+			} else { // assuming top floor
+				this.buttons = new FloorButton[] {new FloorButton(Direction.DOWN)};
+			}
+		} else {
+			this.buttons = new FloorButton[] {new FloorButton(Direction.UP), new FloorButton(Direction.DOWN)};
+		}
+	}
+	
+	
+}
+
+class FloorButton {
+	private Direction name;
+	private boolean pressed;
+	
+	public FloorButton(Direction direction) {
+		this.name = direction;
+		this.pressed = false;
+	}
+	
+	public void pressButton() {
+		pressed = true;
+	}
+	public void clearButton() {
+		pressed = false;
+	}
+	
+	public boolean ispressed() {
+		return pressed;
+	}
+	
+	public Direction getName() {
+		return name;
 	}
 }
