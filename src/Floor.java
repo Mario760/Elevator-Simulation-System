7b301ccbecd366165830158952690c39f23d459e
campbelldw <campdw@rogers.com>
2021-02-03 15:51:32 -0500

    Floor constructor made

--- src/Floor.java
+++ src/Floor.java
@@ -2,4 +2,9 @@
 public class Floor {
 	private int floorNumber;
 	private FloorButton[] buttons;
+	
+	public Floor(int floorNumber, FloorButton[] buttons) { // a top floor variable is needed to know if the sub system is on the top floor
+		this.floorNumber = floorNumber;
+		this.buttons = buttons;
+	}
 }
